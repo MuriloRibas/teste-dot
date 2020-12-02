ca11cbbe3e71760aa072f2e3ef866eb375e4531d
MuriloRibas <muriloribasss@hotmail.com>
2020-12-02 19:05:26 -0300

    Created mobile xs version.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,9 @@ const open      = require('gulp-open');
 const configs = {
     paths: {
         app: {
-            html: './app/*.html'
+            html: './app/*.html',
+            css: './app/css/*.css',
+            images: './app/images/*.png'
         },
         dist: './dist'
     }
@@ -25,6 +27,17 @@ gulp.task('html', () => {
         .pipe(connect.reload())
 });
 
+gulp.task('css', () => {
+    return gulp.src(configs.paths.app.css)
+        .pipe(gulp.dest(configs.paths.dist))
+        .pipe(connect.reload())
+});
+
+gulp.task('images', () => {
+    return gulp.src(configs.paths.app.images)
+        .pipe(gulp.dest('dist/images'));
+});
+
 gulp.task('open', () => {
     gulp.src('dist/index.html')
         .pipe(open({ uri: 'http://localhost:3001' }))
@@ -32,8 +45,9 @@ gulp.task('open', () => {
 
 gulp.task('watch', () => {
     gulp.watch(configs.paths.app.html, gulp.series('html'));
-
+    gulp.watch(configs.paths.app.css, gulp.series('css'));
+    gulp.watch(configs.paths.app.images, gulp.series('images'));
 });
 
 
-gulp.task('default', gulp.parallel('html', 'connect', 'open', 'watch' ));
+gulp.task('default', gulp.parallel('html', 'css', 'images', 'connect', 'open', 'watch' ));
